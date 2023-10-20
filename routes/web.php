bc1637fdbfe01d3ba9ac93e98378b5fd2cac7341
John Doe <fersonpriadi99@gmail.com>
2023-10-20 21:59:18 +0700

    fitur master all (view only)

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MasterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,10 @@ Route::get('/', function () {
 Route::get('/login',[LoginController::class, 'index'])
 ->name('login')
 ->middleware('guest');
+
+Route::get('/master',[MasterController::class, 'index'])
+->name('master')
+->middleware('auth');
 
 Route::post('/login',[LoginController::class, 'authenticate'])
 ->name('kirim-data-login');
